166780a6c9fc46198784a52cfc117d2c2c89ef9c
Marvin Wachira <marvinwachira6@gmail.com>
2023-05-18 15:00:00 +0300

    Updates

--- app/src/main/java/com/example/myapp/ChamaHomeFragment.java
+++ app/src/main/java/com/example/myapp/ChamaHomeFragment.java
@@ -14,52 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChamaHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChamaHomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ChamaHomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChamaHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChamaHomeFragment newInstance(String param1, String param2) {
-        ChamaHomeFragment fragment = new ChamaHomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     private FloatingActionButton fab,fab2,fab1,fab3,fab4,fab5;
     private Animation fabOpen;
